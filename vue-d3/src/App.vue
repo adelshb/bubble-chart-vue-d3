fb0abbbd42f6fb3989cd8f9c3339b7026e9cd8e8
adelshb <adel.sohbi@gmail.com>
2020-01-03 09:56:11 +0900

    prepare dataviz

--- vue-d3/src/App.vue
+++ vue-d3/src/App.vue
@@ -1,18 +1,64 @@
-<template>
+<template v-if="benchmark_data.length >0">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chart 
+      :benchmark_data="benchmark_data"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as d3 from "d3";
+
+import Chart from '@/components/Chart.vue'
+//import Legend from '@/components/Legend.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Chart,
+    //Legend,
+  },
+  data: function(){
+    return {
+      data_loaded: false,
+      benchmark_data: [],
+    }
+  },
+  created: function() {
+    var that = this 
+
+    d3.csv("benchmark.csv",
+      function(data) {  
+        that.benchmark_data.push(data)
+      }
+    );
+
+    console.log("creatingting parent: " , this.benchmark_data.length)
+    this.data_loaded = true
+    console.log(this.data_loaded)
+  },
+  computed: {
+    // input_size: function(){
+    //   let input_size = this.benchmark_data.map(a => a.input_size).filter((x, i, a) => a.indexOf(x) == i).sort(function(a, b){return parseFloat(a)-parseFloat(b)});
+    //   return input_size
+    // },
+    // colors: function(){
+    //   let temp = this.benchmark_data.map(a => parseFloat(a.input_size)).filter((x, i, a) => a.indexOf(x) == i).sort(function(a, b){return a-b});
+    //   let color_range = temp.map(x => d3.interpolateOranges((x-temp[0])/(temp[temp-1]-temp[0])));
+    //   let input_size = this.benchmark_data.map(a => a.input_size).filter((x, i, a) => a.indexOf(x) == i).sort(function(a, b){return parseFloat(a)-parseFloat(b)});
+
+    //   //Set the color for each region
+    //   let colors = d3.scaleOrdinal()
+    //           .domain(input_size)
+    //           .range(color_range);
+    //   return colors
+    // },
+    
+    // param_dom: function(){
+    // let param_dom = d3.extent(this.benchmark_data, function(d) { return parseFloat(d.parameters) ; })
+    // return param_dom
+    // }
+  },
 }
 </script>
 
---
+++ vue-d3/src/components/Chart.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h1>Chart</h1>
+
+    <div v-if="benchmark_data.length >0">
+
+      <div id="chart">
+      </div>
+      {{ benchmark_data.length }}
+    </div>
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  name: 'Chart',
+  props: {
+    benchmark_data: Array,
+    },
+  data: function() {
+    return {
+      settings: {
+        margin: {
+          top: 20, 
+          right: 10, 
+          bottom: 40, 
+          left: 70
+        },
+      width: 600,
+      height: 400,
+      opacityCircles: 0.5,
+      },
+      svgContainer: Object,
+      chartWrapper: Object,
+    }
+  },  
+  mounted: function() {
+    this.svgContainer = d3.select("#chart")
+      .append("svg")
+      .attr("width", (this.settings.width + this.settings.margin.left + this.settings.margin.right))
+      .attr("height", (this.settings.height + this.settings.margin.top + this.settings.margin.bottom))
+    this.svgContainer.append("g")
+      .attr("transform", "translate(" + this.settings.margin.left + "," + (this.settings.height + this.settings.margin.top) + ")")
+      .call(d3.axisBottom(this.xScale))
+    console.log("mounting child")
+    // this.svgContainer.append("g")
+    //   .attr("transform", "translate(" + this.settings.margin.left + "," + this.settings.margin.top + ")")
+    //   .call(d3.axisLeft(this.yScale));
+
+    // this.chartWrapper = this.svgContainer
+    //   .append("g")
+    //   .attr("class", "chordWrapper")
+    //   .attr("transform", "translate(" + this.settings.margin.left + "," + this.settings.margin.top + ")");
+    },
+  computed: {
+    avg_dom: function(){
+      console.log(this.benchmark_data.length)
+      var dom = d3.extent(this.benchmark_data, function(d) { return parseFloat(d.avg) ; })
+      console.log("computing avg dom", dom)
+      return dom
+    },
+    xScale: function() {
+      let xScale = d3.scaleLinear()
+      .range([0, this.settings.width])
+      //.domain([this.avg_dom[0], this.avg_dom[1]])
+      .domain(this.avg_dom)
+      return xScale
+    },
+    yScale: function() {
+      let yScale = d3.scaleLinear()
+      .range([0, this.settings.height])
+      //.domain(d3.extent(this.benchmark_data, function(d) { return parseFloat(d.avg) ; }));
+      .domain([0, 4000])
+      return yScale
+    },
+    rScale: function (){
+      let rScale = d3.scaleSqrt()
+        .domain(d3.extent(this.benchmark_data, function(d) { return 5*d.depth_multiplier;}));
+      return rScale
+    }
+  }
+}
+</script>
